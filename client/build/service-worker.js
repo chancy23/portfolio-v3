5eaea3017e4d633060c0e15fdbdc9564e143c9da
Chancy <chancyleath@hotmail.com>
2019-11-19 19:22:52 -0700

    fixed build path in server file

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bc1d52082a607e011c28a866ccc3d51.js"
+  "/precache-manifest.a16a77dcf16d5847cbb0325782b8244b.js"
 );
 
 self.addEventListener('message', (event) => {
